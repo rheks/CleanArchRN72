941fbc0adebc4d4e766af3cc5b1f2d62bb83a407
Andre Kautsar <Andre.Kautsar@contracted.sampoerna.com>
2024-08-20 23:49:14 +0700

    Add initial home screen

---
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen: React.FC = () => {
+    return (
+        <View>
+            <Text styles={{ color: 'red' }}>Home Screen</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
