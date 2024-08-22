9bba970efadc10296215c4fa4b0f69898229d2ba
Andre Kautsar <Andre.Kautsar@contracted.sampoerna.com>
2024-08-22 20:03:00 +0700

    Integration reduxt with react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import NavigationComponent from "@presentation/components/shared/NavComponent";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from "react-redux";
+import { store } from "@application/redux/store";
 
 function App(): JSX.Element {
 	console.log("=== start App ===")
 
 	return (
-		<SafeAreaProvider>
-			<NavigationComponent />
-		</SafeAreaProvider>
+		<StrictMode>
+			<Provider store={store}>
+				<SafeAreaProvider>
+					<NavigationComponent />
+				</SafeAreaProvider>
+			</Provider>
+		</StrictMode>
 	);
 }
 
